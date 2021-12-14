688ab04e14f0ce949d3f82a20f8e864a238c91b0
israelpina004 <90664097+israelpina004@users.noreply.github.com>
2021-12-14 11:02:41 -0500

    Create server.c

---
+++ server.c
@@ -0,0 +1,15 @@
+
+#include "pipe_networking.h"
+
+int main() {
+
+  int to_client;
+  int from_client;
+
+  from_client = server_handshake( &to_client );
+  
+  char input[100];
+  
+  while(1) {
+    
+}
