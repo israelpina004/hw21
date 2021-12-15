20b66d27cf307acff08668a01160549d0f2c47a9
israelpina004 <90664097+israelpina004@users.noreply.github.com>
2021-12-14 20:43:59 -0500

    Update client.c

--- client.c
+++ client.c
@@ -1,7 +1,4 @@
-include "pipe_networking.h"
-
 #include "pipe_networking.h"
-
 
 int main() {
 
@@ -9,14 +6,19 @@ int main() {
   int from_server;
 
   from_server = client_handshake( &to_server );
-  
+
   char input[100];
-  
+
   while(1) {
     printf("Enter a string: ");
     fgets(input, sizeof(input), stdin);
-    
+
     input[strcspn(input, "\n")] = '\0';
     write(to_server, input, sizeof(input));
-    
+
+    read(from_server, input, sizeof(input));
+    printf("New string: %s\n", input);
+  }
+
+  return 0;
 }
